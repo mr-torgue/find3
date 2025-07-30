deffca184a492a06536f310a3de6987e69503c09
Abi <144218109+aby1602@users.noreply.github.com>
2025-07-30 05:28:51 +0000

    Update learn.py
    numpy.num added

--- server/ai/src/learn.py
+++ server/ai/src/learn.py
@@ -364,14 +364,27 @@ class AI(object):
                 self.logger.debug("Mean per column: %s" % (self.mean_per_ap))
                 # === End of mean_per_ap computation ===
                 '''
-                x = numpy.where(x == self.default_value, numpy.nan, x)
+               ''' x = numpy.where(x == self.default_value, numpy.nan, x)
                 rooms = numpy.unique(y)
                 for room in rooms:
                 	mask = y == room
                 	x_room = x[mask]
                 	room_means = numpy.nanmean(x_room, axis=0)
+                    if numpy.isnan(x_room).all(): continue
                 	x[mask] = numpy.where(numpy.isnan(x_room), room_means, x_room)
                 	self.logger.debug("Mean per column: %s for room %d" % (room_means, room))
+                 '''
+            
+            x = numpy.where(x == self.default_value, numpy.nan, x)
+            rooms = numpy.unique(y)
+            for room in rooms:
+                mask = y == room
+                x_room = x[mask]
+                if numpy.isnan(x_room).all(): continue  # Skip if room has only default values
+                room_means = numpy.nanmean(x_room, axis=0)
+                x[mask] = numpy.where(numpy.isnan(x_room), room_means, x_room)
+                self.logger.debug("Mean per column: %s for room %d" % (room_means, room))
+
         except Exception as e:
             print("An exception occurred: %s" % (e))
         self.logger.debug("x: %s\ncontains nan: %s" % (x, numpy.isnan(x).any()))
