711df83b795ac54e7aef6d7d70de1bb7452afb86
Abi <144218109+aby1602@users.noreply.github.com>
2025-09-09 15:52:08 +1000

    Update learn.py

--- server/ai/src/learn.py
+++ server/ai/src/learn.py
@@ -23,6 +23,8 @@ import os  # [Abi, 2025-09-03] For config file path handling
 import sys, platform
 import sklearn
 
+print("09 SEPT)
+
 # create logger with 'spam_application'
 logger = logging.getLogger('learn')
 logger.setLevel(logging.DEBUG)
@@ -1412,6 +1414,7 @@ def do():
                     k, g, len(set(known_groups[k]).intersection(guessed_groups[g])))
 
 '''''
+
 
 
 
